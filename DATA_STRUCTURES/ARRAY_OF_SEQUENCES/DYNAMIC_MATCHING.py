c24fc11dd6e0ecfa7b535cd50bbbfa6ff1fd4f94
depicted-candela <nicalcoca@gmail.com>
2024-02-27 11:46:29 -0500

    Flat sequences and dynamic matching better coded and explained

--- DATA_STRUCTURES/ARRAY_OF_SEQUENCES/DYNAMIC_MATCHING.py
+++ DATA_STRUCTURES/ARRAY_OF_SEQUENCES/DYNAMIC_MATCHING.py
@@ -1,17 +1,52 @@
-## -> Comment
 class DynamicMatching:
-    def __init__(self, a, b, c, *values):
-        self.PAIRED_MATCHING = 1
-        self.LEFT_MATCHING   = 2
-        self.RIGHT_MATCHING   = 3
-        self.packaged_values = (a, b, c, *values)
-    def pair_matching(self, type):
-        if type == self.PAIRED_MATCHING:
-            a, b, c, *values = self.packaged_values
-            print(a, b, c, values)
-        elif type == self.LEFT_MATCHING:
-            a, b, *values = self.packaged_values
-            print(a, b, values)
-        elif type == self.RIGHT_MATCHING:
-            a, b, c, d, values = self.packaged_values
-            print(a, b, c, d, values)
+    """Example for tuple matching
+    """
+    def __init__(self, a : int, b : int, c : int, *values : int) -> None:
+        """Initializes the class to unpackage values
+
+        Args:
+            a int: The first argument
+            b int: The second argument
+            c int: The third argument
+            *args int: The remaining arguments to be dynamically unpackaged
+        """
+        self.packaged_values    = (a, b, c, *values)
+    # The number for unpackaging with the exact pattern
+    def exact_arguments_unpackager(self):
+        """Unpackages arguments in the exact pattern they were provided
+        >>> dm = DynamicMatching(1, 2, 3, 4, 5, 6, 7)
+        >>> dm.exact_arguments_unpackager()
+        1 2 3 [4, 5, 6, 7]
+        """
+        a, b, c, *values = self.packaged_values
+        print(a, b, c, values)
+    def less_arguments_unpackager(self):
+        """Unpackages less arguments than the number of the not optional ones
+        >>> dm = DynamicMatching(1, 2, 3, 4, 5, 6, 7)
+        >>> dm.less_arguments_unpackager()
+        1 2 [3, 4, 5, 6, 7]
+        """
+        a, b, *values = self.packaged_values
+        print(a, b, values)
+    def more_arguments_unpackager(self):
+        """Unpackages less arguments than the number of the not optional ones
+        >>> dm = DynamicMatching(1, 2, 3, 4, 5, 6, 7)
+        >>> dm.more_arguments_unpackager()
+        1 2 3 4 [5, 6, 7]
+        """
+        a, b, c, d, *values = self.packaged_values
+        print(a, b, c, d, values)
+
+def main():
+    dm = DynamicMatching(1, 2, 3, 4, 5, 6, 7)
+    print("\nPair Matching")
+    dm.exact_arguments_unpackager()
+    print("\nLess Matching")
+    dm.less_arguments_unpackager()
+    print("\nMore Matching")
+    dm.more_arguments_unpackager()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
--- DATA_STRUCTURES/ARRAY_OF_SEQUENCES/FLAT_SEQUENCES_MATCH.py
+++ DATA_STRUCTURES/ARRAY_OF_SEQUENCES/FLAT_SEQUENCES_MATCH.py
@@ -1,7 +1,24 @@
-## -> Comment
-class FlatSequencesMatching:
-    def __init__(self, value):
-        match tuple(value):
-            case ['1', *rest]: print("A: ", rest)
-            case ['2', *rest]: print("B: ", rest)
-            case ['3' | '4', *rest]: print("C: ", rest)
+"""Ejemplo de Comparador de Secuencias Planas
+"""
+class PaisNumeroTelefonico:
+    """Comparador de secuencias planas
+    """
+    def __init__(self, numero : str):
+        """Constructor de para determinar el país de un número telefónico dado
+        Args:
+            numero (str): Número del cuál se determina su país origen
+        >>> _ = PaisNumeroTelefonico('573208639711')
+        573208639711 es un número telefónico Colombiano
+        >>> _ = PaisNumeroTelefonico('57320863971')
+        AssertionError: Ese número telefónico no lo registra ningún país
+        """
+        match tuple(numero):
+            case ['5', '7', *rest] if len(rest) == 10: print(f"{numero} es un número telefónico Colombiano")
+            case ['5', '2', *rest] if len(rest) == 10: print(f"{numero} es un número telefónico Mexicano")
+            case ['1', *rest] if len(rest) == 10: print(f"{numero} es un número telefónico estadounidense")
+            case _: assert False, "Ese número telefónico no lo registra ningún país"
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    PaisNumeroTelefonico('573208639711')
