b527d944fc0936c4f27c7defc8d8f38f33902317
depicted-candela <nicalcoca@gmail.com>
2024-02-29 09:12:02 -0500

    Matching patterns

--- DATA_STRUCTURES/ARRAY_OF_SEQUENCES/MATCH_PATTERNS.py
+++ DATA_STRUCTURES/ARRAY_OF_SEQUENCES/MATCH_PATTERNS.py
@@ -12,21 +12,25 @@ class ColombianLot:
         self.condition              = condition
         self.interested_parties     = tuple()
         while(True) :
-            if True if input("There are exist more interested parties? (y, n): ") == 'y' else False: break
-            interested_party_type
+            if (True if input("There are exist more interested parties? (y, n): ") == 'y' else False): break
             interested_party_type_id    = input("Input the interested party type (ti: T. identidad, cc: C. ciudadanía): ")
             if interested_party_type_id != "cc" or interested_party_type_id != "tid": None
             interested_party_names      = self.__structures_names_intuples(input("Input the names: "))
             interested_party_last_names = self.__structures_names_intuples(input("Input the last names: "))
             self.__destructures_names_intuples((interested_party_type_id,
-                                              interested_party_names,
-                                              interested_party_last_names))
-            
+                                                interested_party_names,
+                                                interested_party_last_names))
+
     def __structures_names_in_tuples(interested_party_names: str) -> tuple:
         return tuple(interested_party_names.strip().split())
     
     def __destructures_names_in_tuples(self, interested_party_names_tuple: tuple):
         match interested_party_names_tuple:
-            case (type_id, names, (first_lastname, second_lastname)) if type_id:
+            ## For a corrient name
+            case (str(type_id), names, (first_lastname, second_lastname)) if type_id:
                 self.interested_parties.append(InterestedParty(type, names, (first_lastname, second_lastname)))
-            case (type, names, (first_lastname, _, second_lastname)) if type_id:
+            ## For a ancient name of a woman
+            case (str(type_id), names, (first_lastname, middle_lastname, second_lastname)) if type_id and middle_lastname.lower() == "de":
+                self.interested_parties.append(InterestedParty(type, names, (first_lastname, second_lastname)))
+            ## Ignore the rest of cases
+            case _: return False
