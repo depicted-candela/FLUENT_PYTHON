c7197cbe61583d3159d74acac17447ae19da8fda
depicted-candela <nicalcoca@gmail.com>
2024-02-29 09:46:21 -0500

    Multiple tuples unpackaged

---
+++ DATA_STRUCTURES/ARRAY_OF_SEQUENCES/MATCH_MULTIPLE_UNPACKING.py
@@ -0,0 +1,23 @@
+def function_definition(*args : str) -> dict:
+    """Multiple unpacking from multiple tuples
+    Returns:
+        dict: dictionary with multiple tuples unpacked
+    >>> function_definition('define', ('name', 'par1', 'par2', 'par3'), 'body1', 'body2', 'body3')
+    ('name', (['par1', 'par2', 'par3'], ['body1', 'body2', 'body3']))
+    >>> ## An example with trailing comma to avoid write more cases
+    >>> function_definition('define', ('name', ), 'body1', 'body2', 'body3')
+    ('name', ([], ['body1', 'body2', 'body3']))
+    >>> ## An example with two trailing commans making another case where body is empty
+    >>> function_definition('define', ('name', ), )
+    ('name', ([], []))
+    """
+
+    match args:
+        case ['define', [name, *params], *body] if body:
+            return (name, (params, body))
+        case ['define', [name, *params], *body]:
+            return (name, (params, body))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
